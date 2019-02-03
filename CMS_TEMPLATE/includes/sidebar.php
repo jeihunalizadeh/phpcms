7ee8327cb8da00fe41070b5f40eabe2929ae382a
Jeihun Alizadeh <jeyhun.alizadeh@gmail.com>
2019-02-03 18:11:11 +0000

    Created new post for testing, Added new file to search.php for Custom Search Engine and added for to the sidebar.php for getting result from db

--- CMS_TEMPLATE/includes/sidebar.php
+++ CMS_TEMPLATE/includes/sidebar.php
@@ -1,18 +1,21 @@
- <div class="col-md-4">
 
+ <div class="col-md-4">
+   
+        <form action="search.php" method="POST">
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
                     <div class="input-group">
-                        <input type="text" class="form-control">
+                        <input name="search" type="text" class="form-control">
                         <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
+                            <button name="submit" class="btn btn-default" type="submit">
                                 <span class="glyphicon glyphicon-search"></span>
                         </button>
                         </span>
                     </div>
                     <!-- /.input-group -->
                 </div>
+                </form>
 
                 <!-- Blog Categories Well -->
                 <div class="well">
